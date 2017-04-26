5808a20d7f869f7f8915169ec850f35e87648dd2
R Sánchez <r@renems.com>
2017-04-25 23:09:42 -0500

    renamed packages

--- src/Examples/JGraphT.java
+++ src/Examples/JGraphT.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Examples;
+package examples;
 
 import org.jgrapht.UndirectedGraph;
 import org.jgrapht.alg.EulerianCircuit;
